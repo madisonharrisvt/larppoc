27a576f7fb203a312222ed4aa8bb56e7538f561f
Madison Harris <madisonharrisvt@gmail.com>
2018-01-20 10:37:06 -0800

    init

---
+++ larp-poc-api/Controllers/CharactersController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using larp_poc_api.Models;
+
+namespace larp_poc_api.Controllers
+{
+    public class CharactersController : ApiController
+    {
+        private readonly Character[] _characters = new Character[]
+        {
+            new Character { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Character { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Character { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
+
+        public IEnumerable<Character> GetAllCharacters()
+        {
+            return _characters;
+        }
+
+        public IHttpActionResult GetCharacter(int id)
+        {
+            var character = _characters.FirstOrDefault((p) => p.Id == id);
+            if (character == null)
+            {
+                return NotFound();
+            }
+            return Ok(character);
+        }
+    }
+}
---
+++ larp-poc-api/Models/Character.cs
@@ -0,0 +1,10 @@
+﻿namespace larp_poc_api.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
