53025c430236dae696aa3038e543b1885d87cb2c
Madison Harris <madisonharrisvt@gmail.com>
2018-02-24 16:06:34 -0800

    Madison: got api to work with angular ui

---
+++ larp-poc-api/Controllers/CharacterSearchController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace larp_poc_api.Controllers
+{
+    public class CharacterSearchController : ApiController
+    {
+        [HttpGet]
+        public List<Dictionary<string, string>> GetByPartialName(string id)
+        {
+            var query = $"SELECT * FROM tblCharacter WHERE name LIKE '%{id}%' ";
+
+            var results = MSSQL.ExecuteMsSqlQuery(query);
+
+            return results;
+        }
+    }
+}
